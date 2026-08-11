70f0b6139bd77f26edacc4987347cb8bd37dd020
Dương Đình Kiên <ddkien05@gmail.com>
2026-08-11 08:00:10 +0700

    Fix project structure

--- ChatTCP.Common/Payloads/TextMessagePayload.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace ChatTCP.Common.Payloads;
-
-public class TextMessagePayload
-{
-    public string MessageId { get; set; } = Guid.NewGuid().ToString();
-    public string Content { get; set; } = string.Empty; // Nội dung bao gồm cả Emoji UTF-8
-}
