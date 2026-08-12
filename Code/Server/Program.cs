e158317f00768f919364a80e143148f990bb7aa4
Duong Dinh Kien <ddkien05@gmail.com>
2026-08-12 16:47:45 +0700

    update server

--- Code/Server/Program.cs
+++ Code/Server/Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ChatTCP.Sever
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClientManager clientManager = new ClientManager();
+            ChatServer server = new ChatServer(clientManager);
+
+            server.Start();
+
+            Console.WriteLine("Nhan enter de dung sever...");
+            Console.ReadLine();
+
+            server.Stop();
+        }
+    }
+}
