bf2e33bbb74d0a8677cd94d1b22db0d33dcdeae9
Duong Dinh Kien <ddkien05@gmail.com>
2026-08-14 13:27:08 +0700

    initialize TcpListener, add AcceptLoop

--- Code/Client/Program.cs
+++ Code/Client/Program.cs
@@ -1,1 +1,30 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ChatTCP.Client
+{
+  // Test ket noi Server
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpClient client = new TcpClient();
+            client.Connect("127.0.0.1", 8888);
+            Console.WriteLine("Da ket noi toi server, go exit de thoat:");
+
+            NetworkStream stream = client.GetStream();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "exit") break;
+
+                byte[] data = Encoding.UTF8.GetBytes(input);
+                stream.Write(data, 0, data.Length);
+            }
+
+            client.Close();
+        }
+    }
+}
--- Code/Server/Clientmanager.cs
+++ Code/Server/Clientmanager.cs
@@ -1,10 +1,61 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Net.Sockets;
 
-namespace Server
+namespace ChatTCP.Sever
 {
-    internal class Clientmanager
+ 
+    public class ClientManager
     {
+        private readonly List<TcpClient> _clients = new List<TcpClient>();
+        private readonly object _lock = new object(); 
+
+        public void Add(TcpClient client)
+        {
+            lock (_lock)
+            {
+                _clients.Add(client);
+            }
+            Console.WriteLine("[ClientManager] Thêm client. Tổng số hiện tại: " + Count);
+        }
+
+        public void Remove(TcpClient client)
+        {
+            lock (_lock)
+            {
+                _clients.Remove(client);
+            }
+
+            try
+            {
+                client.Close(); 
+            }
+            catch
+            {
+                
+            }
+
+            Console.WriteLine("[ClientManager] Xoá client. Tổng số hiện tại: " + Count);
+        }
+
+      
+        public List<TcpClient> GetAll()
+        {
+            lock (_lock)
+            {
+                return new List<TcpClient>(_clients);
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    return _clients.Count;
+                }
+            }
+        }
     }
 }
