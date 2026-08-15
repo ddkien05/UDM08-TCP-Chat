12f04c8017cfd077e223b5cd6dfcf51edf51b16b
Duong Dinh Kien <ddkien05@gmail.com>
2026-08-15 15:06:58 +0700

    clientmanager: remove client

--- Code/Server/Clientmanager.cs
+++ Code/Server/Clientmanager.cs
@@ -8,7 +8,7 @@ namespace ChatTCP.Sever
     public class ClientManager
     {
         private readonly List<TcpClient> _clients = new List<TcpClient>();
-        private readonly object _lock = new object(); // khóa để nhiều Thread không sửa cùng 1 lúc 
+        private readonly object _lock = new object(); 
 
         public void Add(TcpClient client)
         {
@@ -18,7 +18,26 @@ namespace ChatTCP.Sever
             }
             Console.WriteLine("Them client . Tong so client hien tai: " + Count);
         }
+        public void Remove(TcpClient client)
+        {
+            lock (_lock)
+            {
+                _clients.Remove(client);
+            }
 
+            try
+            {
+                client.Close(); 
+            }
+            catch
+            {
+                
+            }
+
+            Console.WriteLine("Xoa client . Tong so client hien tai la: " + Count);
+        }
+
+       
         public int Count
         {
             get
