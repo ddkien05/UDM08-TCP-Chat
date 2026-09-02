31406ea536711f5590f68a2b9b997de6a65592f5
Duong Dinh Kien <ddkien05@gmail.com>
2026-09-02 15:30:59 +0700

    feat(data): add repository interfaces

---
+++ Code/Server/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ChatTCP.Server.Networking;
+using ChatTCP.Server.Services;
+namespace ChatTCP.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClientManager clientManager = new ClientManager();
+            ChatServer server = new ChatServer(clientManager);
+
+            server.Start();
+
+            Console.WriteLine("Nhấn Enter để dừng server...");
+            Console.ReadLine();
+
+            server.Stop();
+        }
+    }
+}
