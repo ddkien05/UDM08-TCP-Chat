1d55e14c93a7720249210dd7536ffc187ba0c700
thuy-ntt <ellynguyen20032006@gmail.com>
2026-09-03 21:14:49 +0700

    feat(protocol): chuan hoa goi tin va format tin nhan

--- Code/Server/Chatserver.cs
+++ Code/Server/Chatserver.cs
@@ -3,7 +3,7 @@ using System.Net;
 using System.Net.Sockets;
 using System.Threading;
 
-namespace ChatTCP.Sever
+namespace ChatTCP.Server
 {
 
     public class ChatServer
--- Code/Server/Clientmanager.cs
+++ Code/Server/Clientmanager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Net.Sockets;
 
-namespace ChatTCP.Sever
+namespace ChatTCP.Server
 {
 
     public class ClientManager
